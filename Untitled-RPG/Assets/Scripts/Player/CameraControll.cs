1064dc3193413aab5fd215326a1175c648b892be
Grant Oganyan <grant1599@mail.ru>
2020-04-08 00:41:06 -0400

    Added character controlls
    - Walk, run, jump, double jump
    - Camera rotations

---
+++ Untitled-RPG/Assets/Scripts/Player/CameraControll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    public Transform Player;
+    float cameraX;
+    float cameraY;
+
+    public Vector3 offset;
+    public float mouseSensitivity = 1;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        cameraX += Input.GetAxis("Mouse X") * mouseSensitivity;
+        cameraY += Input.GetAxis("Mouse Y") * mouseSensitivity;
+    }
+
+    void LateUpdate()
+    {
+        Quaternion rotation = Quaternion.Euler(cameraY, cameraX, 0);
+        transform.position = Player.position + rotation * offset;
+        transform.LookAt(Player.position);
+    }
+}
---
+++ Untitled-RPG/Assets/Scripts/Player/PlayerControlls.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerControlls : MonoBehaviour
+{
+    [Header("Movement")]
+    public int currentSpeed = 0;
+    public int walkSpeed = 5;
+    public int runSpeed = 10;
+    public float jumpHeight = 10;
+    public bool toggleRunning = false;
+
+    public float gravity = -12;
+    public float velocityY;
+    Vector3 velocity;
+
+    CharacterController controller;
+
+    Camera playerCamera;
+
+    public bool doubleJump;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.CapsLock))
+            toggleRunning = !toggleRunning;
+    }
+
+    void Movement()
+    {
+        if (Input.GetButton("Run"))
+        {
+            if (!toggleRunning)
+                currentSpeed = runSpeed;
+            else
+                currentSpeed = walkSpeed;
+        } else
+        {
+            if (toggleRunning)
+                currentSpeed = runSpeed;
+            else
+                currentSpeed = walkSpeed;
+        }
+
+        velocityY += gravity * Time.deltaTime;
+        velocityY = Mathf.Clamp(velocityY, gravity * 3, -gravity * 3);
+
+        velocity = transform.forward * currentSpeed * Input.GetAxis("Vertical") + transform.right * currentSpeed * Input.GetAxis("Horizontal") + Vector3.up * velocityY;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+
+        if (controller.isGrounded)
+            doubleJumped = false;
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, playerCamera.transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+
+    bool doubleJumped = false;
+    void Jump ()
+    {
+        if (controller.isGrounded)
+        {
+            velocityY = Mathf.Sqrt(-2 * gravity * jumpHeight);
+        } else
+        { 
+            if (doubleJump && !doubleJumped)
+            {
+                doubleJumped = true;
+                velocityY = Mathf.Sqrt(-2 * gravity * jumpHeight);
+            }
+        }
+    }
+}
